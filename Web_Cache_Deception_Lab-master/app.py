e345c86b978f667a3cd47f1e0b3895311da4f5b6
Enrongkoh <kohenrong@gmail.com>
2022-09-08 17:37:43 +0800

    update port and ip

--- Web_Cache_Deception_Lab-master/app.py
+++ Web_Cache_Deception_Lab-master/app.py
@@ -65,4 +65,4 @@ def main():
     return redirect("/login")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000 , debug=True) #192.168.137.1
