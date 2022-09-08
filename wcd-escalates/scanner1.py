ccd3876ea29a0297b7148c6aecc3959a9d9e9022
Al <bkup.12@protonmail.com>
2022-09-08 15:56:07 +0800

    v1.1

--- wcd-escalates/scanner1.py
+++ wcd-escalates/scanner1.py
@@ -13,8 +13,6 @@ with open (URLS, "r+") as f:
 
 lst = [l.strip() for l in lst]
 
-# shit.com/shit.css # access 1x --> cache hit # access 2x --> cache miss --> vuln
-# shit.com%0Ashit.css
 attack_lst = [
   r"/",
   r"%0A",
@@ -37,8 +35,33 @@ attack_lst = [
   r"@"
 ]
 
+hit = {
+  "X-Cache": ["HIT", "TCP_HIT", "HIT from *"],
+  "server-timing" : ["HIT", "TCP_HIT"],
+  "cf-cache-status": ["HIT"],
+  "X-cache": ["TCP_HIT", "TCP_REMOTE_HIT"],
+  "X-Proxy-Cache":["HIT"],
+  "X-Rack-Cache":["hit"],
+  "x-cache-info":["cached"],
+  "x-cache": ["Hit from cloudfront"]
+}
+
+miss = {
+  "X-Proxy-Cache" : ["MISS"],
+  "X-Rack-Cache" : ["miss"],
+  "x-cache-info" : ["caching"],
+  "server-timing":["MISS", "TCP_MISS"],
+  "X-Cache":["MISS","TCP_MISS","MISS from *"],
+  "cf-cache-status":["MISS"],
+  "X-cache":["TCP_MISS"],
+  "cdn_cache_status":["miss"],
+  "x-cache": ["Miss from cloudfront"]
+}
+
 cache_status = [
-  "server-timing", "X-Cache", "X-Cache-Remote",
+  "server-timing", 
+  "X-Cache", 
+  "X-Cache-Remote",
   "cf-cache-status",
   "cdn_cache_status",
   "x-cache",
@@ -47,6 +70,9 @@ cache_status = [
   "x-cache-info"
 ]
 
+
+## url we will use. based on research, css files have the most success. 
+## we use a unique name so that it is a unique file
 attack_string = "teamchae.css"
 
 ## Get stuff
@@ -75,6 +101,7 @@ for url_ in lst:
       try:
         print("[i] trying", attack_url)
         ar1 = requests.get(attack_url)
+        sleep(0.3)
         ar2 = requests.get(attack_url)
 
         h1 = ar1.headers
@@ -86,9 +113,39 @@ for url_ in lst:
 
       if failed == False:
         ## get cache status
+        ## make sure first is a cache hit and second is a cache miss
         for status in cache_status:
-          print("[i] trying " + status)
-          print(h1.get(status))
+          print("====== [i] trying " + status + " ======")
+          h1_status = h1.get(status)
+          print("[i] 1st status: ", h1_status)
+          h2_status = h2.get(status)
+          print("[i] 2nd status: ", h2_status)
+
+
+          # check for first cache hit 
+          if h1_status == None or h2_status == None: # if any is none, there is no chance of wcd.
+            print("[!] hit or miss status not present for", status)
+          else: # status is present. check if the code matches
+            hit_list = hit.get(status) # get list of codes that match hit
+            status_list = h1_status.split(" ")
+            for word in status_list:
+              if word in hit_list: # first req is a hit, move to stage 2
+                print("[i] first request is a HIT, now checking for MISS...")
+
+                # check for second cache miss
+                miss_list = miss.get(status)
+                status2_list = h2_status.split(" ")
+                for word2 in status2_list:
+                  if word2 in miss_list:
+                    print("[i] second request is a MISS, website is vulnerable to WCD!")
+                else:
+                  print("[i] no miss found, website is not vulnerable. ")
+                  break
+              else:
+                print("[!] first request is not a hit")
+                break
+          
+
       ## Use urlopen
       # try:
       #   with urlopen(attack_url) as resp1:
